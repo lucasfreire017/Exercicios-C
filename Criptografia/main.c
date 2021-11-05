d8db374ac2879c973b4b2c8e27f7242d37b10138
Lucas Freire <42319655+lucasfreire017@users.noreply.github.com>
2021-11-04 21:48:57 -0300

    Final Script

--- Criptografia/main.c
+++ Criptografia/main.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
+    FILE *contc;
+    char d;
+    int cont=0;
+    
+    contc = fopen("mensagem.txt", "r");
+    
+    if(contc == NULL)
+    {
+        printf("\nNao foi possivel abrir o arquivo, por favor, renomeie o mesmo para \"mensagem.txt\" ");
+        return 1;
+    }
+    
+    else
+    {
+    while(!feof(contc))
+    {
+        fscanf(contc, "%c", &d);
+        cont++;
+    }
+    cont--;
+    }
+    
+    
+
+
+//----------------------------------------------------------------------------------------------------------------
+    
+    
+    
+    
     FILE *ler;
     FILE *escreve;
     char c;
-    int resposta, chave;
-
-
-    printf("Informe o que deseja fazer\n 1 - Criptografar\n 2 - Descriptografar\n 3 - Sair\n ==>"); // Menus
-    scanf("%i", &resposta); // Leitura da repsosta do usúario
-
+    int resposta, i;
     
-    if(resposta == 1)
+    printf("Informe o que deseja fazer\n 1 - Criptografar\n 2 - Descriptografar\n 3 - Sair\n ==>");
+    scanf("%d", &resposta);
+    
+    
+    if(resposta==1)
         {
-        printf("Digite a chave de criptografia:");
-        scanf("%i", chave);
         ler = fopen("mensagem.txt", "r");
         escreve = fopen("criptografa.txt", "w");
-        
-            while(!feof(ler))
-            {
+    
+             for(i=1; i<=cont; i++)
+             {
                 fscanf(ler, "%c", &c);
                 
-                if(c>=33 && c<=47)
+                if((c>=32 && c<=47) || (c=='\n'))
                 {
                     c = c + 1;
                 }
@@ -40,30 +66,31 @@ int main()
                 {
                     c = c + 4;
                 }
-                else
+                else if (c>=139 && c<=250)
                 {
-                    c = c - 1;
+                    c = c + 5;
                 }
                 
                 fprintf(escreve, "%c", c);
-           }
+             }
+           
            
            fclose(ler);
            fclose(escreve);
+        
         }
   
     else if(resposta==2)
         {
-        printf("Digite a chave de criptografia:");
-        scanf("%i", chave);
+            
         ler = fopen("criptografa.txt", "r");
         escreve = fopen("descriptografa.txt", "w");
         
-            while(!feof(ler))
-            {
+        for(i=1; i<=cont; i++)
+        {
             fscanf(ler, "%c", &c);
             
-            if(c>=34 && c<=48)
+            if((c>=33 && c<=48) || (c==11))
             {
                 c = c - 1;
             }
@@ -79,9 +106,9 @@ int main()
             {
                 c = c - 4;
             }
-            else
+            else if(c>=144 && c<=255)
             {
-                c = c + 1;
+                c = c - 5;
             }
             
             fprintf(escreve, "%c", c);
